6b0d2de52c8370a9b5716da02524f5bd703d2fa3
Yash <yashharale2003@gmail.com>
2023-03-11 19:47:58 +0530

    Objects and EventHandlers

---
+++ WebAppDev/Unit2/JavascriptFundamentals/Events/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// document.querySelector(".btn").addEventListener("click", fun);
+
+// const fun = function() {
+//     // document.body.style.backgroundColor = `blue`
+//     // document.body.style.backgroundColor = 'blue';
+//     console.log("HI");
+//     console.log(10+"11");
+
+//     document.body.style.backgroundColor = `blue`;
+
+
+// }
+
+// console.log(document.querySelector('.btn').textContent);
+
+
+console.log( document.querySelector('.darwin'));
+
+// console.log(elem);
