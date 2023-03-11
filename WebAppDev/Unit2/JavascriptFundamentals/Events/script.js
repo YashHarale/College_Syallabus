c3f389f8307b4881cbd4c3a412d19990d7cc0c13
Yash <yashharale2003@gmail.com>
2023-03-11 20:06:48 +0530

    events

--- WebAppDev/Unit2/JavascriptFundamentals/Events/script.js
+++ WebAppDev/Unit2/JavascriptFundamentals/Events/script.js
@@ -1,21 +1,26 @@
 'use strict';
 
-// document.querySelector(".btn").addEventListener("click", fun);
 
-// const fun = function() {
-//     // document.body.style.backgroundColor = `blue`
-//     // document.body.style.backgroundColor = 'blue';
-//     console.log("HI");
-//     console.log(10+"11");
+const fun = function() {
+    // document.body.style.backgroundColor = `blue`
+    // document.body.style.backgroundColor = 'blue';
+    console.log("HI");
+    console.log(10+"11");
+    
+    document.body.style.backgroundColor = `blue`;  
+}
 
-//     document.body.style.backgroundColor = `blue`;
+function changeColor () {
+    document.body.style.color = `aqua`;
+}
+
+document.querySelector(".btn").addEventListener("click", fun);
+
+document.querySelector(".heading").addEventListener("mouseover", changeColor)
 
 
-// }
-
-// console.log(document.querySelector('.btn').textContent);
+console.log(document.querySelector('.btn').textContent);
 
 
-console.log( document.querySelector('.darwin'));
-
+console.log( document.querySelector('.darwin').textContent);
 // console.log(elem);
